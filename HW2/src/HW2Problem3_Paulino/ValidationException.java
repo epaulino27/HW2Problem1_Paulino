76f78db3de6145d94c0e8bead6f90f6d54fbb884
epaulino27 <143731967+epaulino27@users.noreply.github.com>
2025-11-18 13:02:26 -0500

    Delete HW2/src/HW2Problem3_Paulino directory

--- HW2/src/HW2Problem3_Paulino/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package HW2Problem3_Paulino;
-
-public class ValidationException extends Exception{
-    public ValidationException(String message){
-        super(message);
-    }
-}
--- HW2/src/HW2Problem3_Paulino/Validator.java
+++
@@ -1,7 +0,0 @@
-package HW2Problem3_Paulino;
-
-public interface Validator {
-    void setNextValidator(Validator nextValidator);
-
-    void validate(UserRegistration registration) throws ValidationException;
-}
